f0f025e74e10edd0aea2d2e8e03bd770b2273f1e
Jakub Berkowski <jakub>
2023-04-20 09:59:00 +0200

    Added example of grouping with parentheses.

--- isPhoneNumber.py
+++ isPhoneNumber.py
@@ -11,3 +11,15 @@ mo1 = phoneNumRegex.search('My phone number is 123-123-7368.')
 
 # Display phone number found.
 print('Phone number found: ' + mo1.group())
+
+# Separately group area code and main number with parentheses.
+groupNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo = groupNumRegex.search('My number is 432-109-8765')
+
+# Show each group.
+print(mo.group(1))
+print(mo.group(2))
+print(mo.group())
+
+# Show tuple containing all the groups.
+print(mo.groups())
