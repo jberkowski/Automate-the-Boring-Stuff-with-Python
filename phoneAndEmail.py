4da7dead088553b4170ddeff5c6052e3d4e19508
jberkowski <jakub.berkowski@gmail.com>
2023-12-11 21:50:56 +0100

    Created phoneAndEmail.py.

---
+++ phoneAndEmail.py
@@ -0,0 +1,39 @@
+# phoneAndEmail.py - Finds US phone numbers and email addresses on the clipboard.
+
+import pyperclip, re
+
+# Create phone Regex: 
+
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?               # area code
+    (\s|-|\.)?                       # separator
+    (\d{3})                          # first 3 digits
+    (\s|-|\.)?                       # separator
+    (\d{4})                          # last 4 digits
+    (\s*(ext|x||ext.)\s*(\d{2,5}))?  # extension
+    )''', re.VERBOSE)
+
+# Create email Regex:
+
+emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+       # username
+    @                       # @ symbol
+    [a-zA-Z0-9.-]+          # domain name
+    (\.[a-zA-Z]{2,4})       # dot-something
+    )''', re.VERBOSE)
+
+# Find matches in clipboard text:
+
+text = str(pyperclip.paste())
+
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+# TODO: Copy results to the clipboard.
+
