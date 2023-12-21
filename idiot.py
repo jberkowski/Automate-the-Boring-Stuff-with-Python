aae6c59da1040184409303db42b3de095c60ed7d
jberkowski <jakub.berkowski@gmail.com>
2023-12-21 20:45:17 +0100

    Created idiot.py

---
+++ idiot.py
@@ -0,0 +1,13 @@
+# Asks user a repetitive question unless they answer "no".abs
+
+import pyinputplus as pyip
+
+while True:
+    prompt = 'Want to know how to keep an idiot busy for hours?\n'
+    response = pyip.inputYesNo(prompt)
+    
+    if response == 'no':
+        break
+
+print('Thank you. Have a nice day.')
+
